5107bf1bd7c991b1d3d1b855a24c6c5862f37a74
Enlai Li <enlai.li@mail.mcgill.ca>
2023-02-14 21:32:06 -0500

    l4 start

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Zoo z = new Zoo();
+
+        for (String animal : z) {
+            System.out.println(animal);
+        }
+    }
+}
+
+class Zoo implements Iterable<String> {
+    String[] animals = { "Panda", "Rabbit" };
+
+    public Iterator<String> iterator() {
+        return new ZooIterator();
+    }
+
+    class ZooIterator implements Iterator<String> {
+
+        int index = 0;
+
+        public boolean hasNext() {
+            return index < animals.length;
+        }
+
+        @Override
+        public String next() {
+            String animal = animals[index];
+            index++;
+            return animal;
+        }
+    }
+}
