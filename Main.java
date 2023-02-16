4231958a47f7cdf0461a5fc2237c3b92e0a9f30a
Enlai Li <enlai.li@mail.mcgill.ca>
2023-02-16 17:20:35 -0500

    l7 start

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 import java.util.Iterator;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Zoo z = new Zoo();
-
-        for (String animal : z) {
-            System.out.println(animal);
-        }
+        // create null in optional
+        Optional<String> nullStr = Optional.empty();
+        // turn object into optional, works with null value
+        Optional<String> str = Optional.ofNullable("Hello");
+        // if-else
+        nullStr.ifPresentOrElse(t -> System.out.println(t), () -> System.out.println("String is empty"));
+        System.out.println(nullStr.isEmpty()); // True
+        System.out.println(str.isPresent()); // True
+        // Get the value, doesn't work with empty Optional
+        System.out.println(str.get());
     }
 }
 
